d19b19fdfc961f57e599393aed9b6f859d4aedb0
vienpham2019 <vienpham2019@gmail.com>
2020-02-08 23:06:24 -0600

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,16 @@
 class Character < ActiveRecord::Base
-  
-end
+  belongs_to :actor 
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def build_show(name:)
+    self.show = Show.create(:name => name)
+  end
+
+  def build_network(call_letters:)
+    self.network = NetWork.create(:call_letters => call_letters)
+  end
+end 
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
+
+  def actors_list 
+    actor_ids = Character.all.where('show_id = ?',self.id).pluck(:actor_id)
+    Actor.all.where('id = ?',actor_ids).pluck(:first_name, :last_name).collect{|name| name.join(" ")}
+  end
+
 end
